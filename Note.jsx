ad7f900cc80bcf342fced99ca26454562f255bd4
suraj719 <71000909+suraj719@users.noreply.github.com>
2021-06-05 13:06:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>Hey,<br />I am suraj, currently learning Full stack Web development and Digital Marketing.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
